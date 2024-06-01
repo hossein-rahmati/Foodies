939124aace50a3fe4623fc07d122d6c234c57259
hossein rahmati <rmthossein@gmail.com>
2024-06-01 09:07:58 +0330

    handle some errors

--- app/meals/error.js
+++ app/meals/error.js
@@ -3,7 +3,7 @@
 export default function Error() {
   return (
     <main className="error">
-      <h1>An error occurred!</h1>
+      <h1>Meal not found!</h1>
       <p>Failed to fetch meals. please try again later.</p>
     </main>
   );
